48c284003f3d9376c629730a5d742bbc6d462c2e
Giorgio Bellisario <72039923+Bellisario@users.noreply.github.com>
2022-10-14 21:13:40 +0200

    PWA support and custom code banner
    Added the app icons to PWA manifest and a custom banner on built JS code.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,21 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 import postcss from './postcss.config.js'
 
+const banner = `/**
+Copyright (c) ${new Date().getFullYear()} Giorgio Bellisario, all rights reserved.
+Code released under the MIT license.
+
+Source code available at: https://github.com/Bellisario/musicale
+*/`
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    // prevent Microsoft banner from being added to the bundle
+    legalComments: 'none',
+    // add custom banner
+    banner,
+  },
   build: {
     outDir: './dist/public',
   },
@@ -14,12 +27,34 @@ export default defineConfig({
       name: 'Musicale',
       short_name: 'Musicale',
       theme_color: '#ff9900',
+      background_color: '#2c2a2a',
+      description: 'Musicale, a music streaming service with style.',
+      icons: [
+        {
+          src: 'favicon_60x.png',
+          sizes: '60x60',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_120x.png',
+          sizes: '120x120',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_180x.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon.svg',
+          sizes: '512x512',
+          type: 'image/svg+xml'
+        },
+      ]
     },
     includeAssets: [
       "./fonts/*",
-      // temp fix (finding a better way)
-      "./*.png",
-      "./*.svg",
+      "./logo.svg",
     ],
     devOptions: {
       // enabled: true,
