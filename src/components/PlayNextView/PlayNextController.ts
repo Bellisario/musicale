6249a59e7db930cff6395e8c933a8c5a61b7e865
Bellisario <72039923+Bellisario@users.noreply.github.com>
2023-07-16 11:26:37 +0200

    feat: mediasession actions on PlayNext

--- src/components/PlayNextView/PlayNextController.ts
+++ src/components/PlayNextView/PlayNextController.ts
@@ -59,3 +59,40 @@ async function wantPlay(result: Result) {
 
     currentID.set(id)
 }
+
+if ('mediaSession' in navigator) {
+    navigator.mediaSession.setActionHandler('previoustrack', () => {
+        // if there is no song in playNextList, return
+        if (get(playNextList).length === 0) return;
+
+        const wasPlayingID = get(currentID);
+
+        // find the index of the current playing song
+        const index = get(playNextList).findIndex((result) => {
+            return urlToId(result.url) === wasPlayingID;
+        });
+
+        // if the index is the first index, it means the song is the first song
+        if (index === 0) return;
+
+        // play the previous song
+        wantPlay(get(playNextList)[index - 1]);
+    });
+    navigator.mediaSession.setActionHandler('nexttrack', () => {
+        // if there is no song in playNextList, return
+        if (get(playNextList).length === 0) return;
+
+        const wasPlayingID = get(currentID);
+
+        // find the index of the current playing song
+        const index = get(playNextList).findIndex((result) => {
+            return urlToId(result.url) === wasPlayingID;
+        });
+
+        // if the index is the last index, it means the song is the last song
+        if (index === get(playNextList).length - 1) return;
+
+        // play the next song
+        wantPlay(get(playNextList)[index + 1]);
+    });
+}
